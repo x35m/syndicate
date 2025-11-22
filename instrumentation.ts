bad4b3ea088edcf2bb4007f2479b3b867604586f
digitalcluster25 <digitalcluster25@gmail.com>
2025-11-22 06:54:37 +0100

    Add logging to instrumentation hook

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,11 @@
 export async function register() {
+  console.log('🔧 Instrumentation hook called');
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    console.log('🔧 Initializing cron service...');
     const { initCron } = await import('./lib/cron');
     initCron();
+  } else {
+    console.log('🔧 Skipping cron - not nodejs runtime');
   }
 }
 
