e06dbae2463d8fb854987c782e59299c03e11bef
digitalcluster25 <digitalcluster25@gmail.com>
2025-11-22 11:20:19 +0100

    Add sitemap and robots exclusions to middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,17 @@ export default createMiddleware({
 });
 
 export const config = {
-  // Применять middleware ко всем путям кроме api, _next, favicon
-  matcher: ['/((?!api|_next|favicon.ico).*)']
+  /*
+   * Match all request paths except:
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   * - sitemap.xml (sitemap)
+   * - robots.txt (robots)
+   * - api/* (API routes)
+   */
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)',
+  ],
 };
 
