a93abda2a771eedfcbcee8a1c07efb975c57aae4
digitalcluster25 <digitalcluster25@gmail.com>
2025-11-22 07:04:16 +0100

    Clean up: Remove unused cron code, use system cron instead

--- lib/cron.ts
+++
@@ -1,9 +0,0 @@
-import { CronService } from './services/CronService';
-
-export function initCron() {
-  if (process.env.NODE_ENV === 'production') {
-    const cronService = CronService.getInstance();
-    cronService.start();
-  }
-}
-
--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-const nextConfig: NextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
